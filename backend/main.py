ead2c88424f5a7e6b6f3d793353bc82c9e500fa3
whitecherry <jsh010322@gmail.com>
2021-06-19 22:41:48 +0900

    removed testing

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from flask import *
 import elasticsearch
-import stockcrawler
+import infocrawl
 import datetime
 
 app = Flask(__name__.split('.')[0])
@@ -8,22 +8,19 @@ es=elasticsearch.Elasticsearch()
 
 @app.route('/getStockInfo')
 def getStockInfo():
-    stockId=int(request.args.get('id',-1))
+    stockId=request.args.get('id',-1)
     if stockId==-1:
         abort(404)
-
 
     try:
         stockInfo = es.get('stock-info',stockId)['_source']
         if stockInfo['date']<datetime.date.today().isoformat():
-            stockInfo = stockcrawler.getstockInfo(stockId)
+            stockInfo = infocrawl.getstockInfo(stockId)
             es.index('stock-info',stockInfo,id=stockId)
-        print('here')
     except elasticsearch.exceptions.NotFoundError:
-        stockInfo = stockcrawler.getStockInfo(stockId)
+        stockInfo = infocrawl.getStockInfo(stockId)
         stockInfo['date'] = datetime.date.today().isoformat()
         es.index('stock-info',stockInfo,id=stockId)
-        print('here2')
         
     return stockInfo
 
